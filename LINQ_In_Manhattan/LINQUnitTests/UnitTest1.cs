9c922cd349fd4eef3e0e130f2dd55e75108cdf69
Blaise Clarke <blaiserclarke@gmail.com>
2019-01-17 10:23:00 -0800

    Imported newtonsoft.json, might have created JObject

---
+++ LINQ_In_Manhattan/LINQUnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using LINQ_In_Manhattan;
+
+namespace LINQUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- LINQ_In_Manhattan/LINQ_In_Manhattan/Program.cs
+++ LINQ_In_Manhattan/LINQ_In_Manhattan/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace LINQ_In_Manhattan
 {
@@ -7,6 +10,11 @@ namespace LINQ_In_Manhattan
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            using (StreamReader reader = File.OpenText(@"../../data.json"))
+            {
+                JObject o = (JObject)JToken.ReadFrom(new JsonTextReader(reader));
+                // do stuff
+            }
         }
     }
 }
